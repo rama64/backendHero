218767a688795121eec06ede08e205327295181e
rama64Dev <ramiro.ongaro@devsar.com>
2022-10-19 15:44:49 -0300

    add default springboot code

---
+++ src/main/java/com/examplebackend/exampleBackend/ExampleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.examplebackend.exampleBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplebackend/exampleBackend/ExampleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplebackend.exampleBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
