480082b490dd978115826304b086c867e236c881
rama64Dev <ramiro.ongaro@devsar.com>
2022-10-19 16:36:21 -0300

    add Person entity

--- src/main/java/com/examplebackend/exampleBackend/entity/Person.java
+++ src/main/java/com/examplebackend/exampleBackend/entity/Person.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.examplebackend.exampleBackend.entity;
 
-/**
- *
- * @author rama_
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 50, message = "name filed on Person must have at least 1 character and up to 50")
+    private String name;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "surname filed on Person must have at least 1 character and up to 50")
+    private String surname;
+    
+    @Size(min = 1, max = 50, message = "image filed on Person must have at least 1 character and up to 50")
+    private String image;
+    
     
 }
