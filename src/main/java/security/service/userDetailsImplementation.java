331b32c7c5c68ed7f3e82cf3dc115834698ea8df
rama64Dev <ramiro.ongaro@devsar.com>
2022-10-19 23:22:15 -0300

    add UserDetailsImplementation

---
+++ src/main/java/security/service/userDetailsImplementation.java
@@ -0,0 +1,24 @@
+
+package security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import security.entity.MainUser;
+import security.entity.User;
+
+@Service
+public class userDetailsImplementation implements UserDetailsService {
+    @Autowired
+    UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getByUserName(username).get();
+        return MainUser.build(user);
+    }
+    
+    
+}
