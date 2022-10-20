67adb98e8b19f01a0082a15e1b37d01c9781cb3e
rama64Dev <ramiro.ongaro@devsar.com>
2022-10-19 23:15:09 -0300

    add userService

--- src/main/java/security/service/UserService.java
+++ src/main/java/security/service/UserService.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import security.entity.Rol;
 import security.entity.User;
 import security.repository.UserRepository;
 
